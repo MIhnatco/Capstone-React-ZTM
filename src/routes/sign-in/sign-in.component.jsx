a0a9c8c5cccb373a0311355a12de40d56a1f63fa
Ihliana <Ihnatco@protonmail.com>
2024-04-11 09:18:56 +0200

    Sign up form and controled inputs

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,15 +3,20 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils.js";
 
+import SignUpForm from "../../components/sign-up-form/sing-up-form.component.jsx";
+
 const SignIn = () => {
-  const logGooleUser = async () => {
+  const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign In page</h1>
-      <button onClick={logGooleUser}>Sign In with Google</button>
+      <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+
+      <SignUpForm />
     </div>
   );
 };
